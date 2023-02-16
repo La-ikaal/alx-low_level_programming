59d6d02eec984b4b32274eb5ce594cd38e38118b
La-ikaal <scholasticaidewa@gmail.com>
2023-02-16 08:19:54 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
- 
-	char str1[] = "\"Programming is like building a multilingual puzzle";
+{	 
+char str1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 	return (0);
