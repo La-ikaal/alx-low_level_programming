e58055c32ec9a9a3bbe72644579fa94ed092a291
La-ikaal <scholasticaidewa@gmail.com>
2023-02-16 10:56:50 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %ld byte(s) \n", sizeof(b));
 printf("Size of an int: %ld byte(s) \n", sizeof(a));
 printf("Size of a long int: %ld byte(s) \n", sizeof(c));
 printf("Size of a long long int: %ld byte(s) \n", sizeof(d));
-printf("Size of a float: %ld byte(s) \n", sizeof(e));
+printf("Size of a float: %ld byte(s)", sizeof(e));
 	return (0);
 }
