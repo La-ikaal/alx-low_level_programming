2f531e349175854a9a4b0c099461f72d03e9176f
La-ikaal <scholasticaidewa@gmail.com>
2023-02-17 11:32:52 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char i;
+	for (num =  0; num < 9 ; num++)
+		putchar(num+ '0');
+	for (i = 'a' ; i <= 'f' ; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
