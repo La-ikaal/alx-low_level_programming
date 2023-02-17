836daf6c77418d4a89313ffa0f1d86003b25ed12
La-ikaal <scholasticaidewa@gmail.com>
2023-02-17 11:42:30 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,9 +13,8 @@ int main(void)
 
 	for (num =  0; num < 10 ; num++)
 		putchar(num + '0');
-	for (i = 'a' ; i <= 'f' ; i++) 
+	for (i = 'a' ; i <= 'f' ; i++)	
 		putchar(i);
-
 	putchar('\n');
 	return (0);
 }
