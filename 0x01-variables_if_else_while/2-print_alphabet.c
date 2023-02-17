685da96f37f293117365fe30c40be217fb470ade
La-ikaal <scholasticaidewa@gmail.com>
2023-02-17 05:45:28 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{	
+	char a[26] = {'a','b','c','d','e','f','g','h','i','j','k','l',
+ 		'm','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	char *cptr = a;
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		putchar(cptr[i]);
+		putchar('\n');
+	}
+	return (0);
+}
