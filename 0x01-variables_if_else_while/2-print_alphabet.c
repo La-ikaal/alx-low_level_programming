4ab840c7807e76f14d63f3a0e4580ed8a1abb3b1
La-ikaal <scholasticaidewa@gmail.com>
2023-02-17 05:50:29 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,15 +8,13 @@
  */
 int main (void)
 {	
-	char a[26] = {'a','b','c','d','e','f','g','h','i','j','k','l',
- 		'm','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	char a[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	char *cptr = a;
 	int i;
 
 	for (i = 0; i <= 26; i++)
 	{
 		putchar(cptr[i]);
-		putchar('\n');
 	}
 	return (0);
 }
