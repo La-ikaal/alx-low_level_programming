233f8cb23187afa3b182e60abcbd184b9dfcb154
La-ikaal <scholasticaidewa@gmail.com>
2023-02-17 07:04:51 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,8 +11,6 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
 	putchar(letter);
-	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sring.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
