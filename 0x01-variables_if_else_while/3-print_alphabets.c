5e3054db97f7bbfcb594c2c079a1e17eedaade67
La-ikaal <scholasticaidewa@gmail.com>
2023-02-17 07:55:36 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,3 +6,13 @@
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'a'; letter <= 'Z'; letter++
+		putchar(lette);
+		putchar('\n');
+		return (0);
+}
