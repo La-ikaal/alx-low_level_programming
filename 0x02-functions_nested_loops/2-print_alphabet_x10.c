bf7565a2476cd9b64559fa21cd1d2d919df3cc34
La-ikaal <scholasticaidewa@gmail.com>
2023-02-21 09:04:01 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-        char c;
+	char c;
 	int i;
 	
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (c = 'a' ; c <= 'z' ; c++)
-                _putchar(c);
+	_putchar(c);
 	_putchar('\n');
 	}
 }
