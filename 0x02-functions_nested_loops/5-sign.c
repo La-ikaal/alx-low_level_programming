7e8e44aa39c41c1a999fe1686a7c05d87e62029e
La-ikaal <scholasticaidewa@gmail.com>
2023-02-22 01:51:17 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,8 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n : the number whose sign is printed by print_sign function
- * Return - returns 1 for positive num, -1 for negative num and 0 for 0
+ * @n : the number to be checked
+ * Return : returns 1 for positive num, -1 for negative num and 0 for 0
  */
 int print_sign(int n)
 {
