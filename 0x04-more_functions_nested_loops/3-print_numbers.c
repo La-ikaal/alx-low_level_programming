379ecfe57de280602603308fca6b706157841a8e
La-ikaal <scholasticaidewa@gmail.com>
2023-02-23 09:47:19 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,12 +4,12 @@
 /**
  * print_numbers: prints numbers from 0 to 9
  * @n - number between 0 and 9
- * Return: returns 0
+ * Return: no value returned
  */
 void print_numbers(void)
 {
 	int n;
-
+	
 	for (n = 0; n <= 9 ; n++)
 		putchar(n + '0');
 	putchar('\n');
