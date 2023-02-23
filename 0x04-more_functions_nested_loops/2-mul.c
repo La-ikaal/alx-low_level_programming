75da14c20cfde7a06c0d05366d4ee3f4408216ea
La-ikaal <scholasticaidewa@gmail.com>
2023-02-23 07:31:37 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * mul - multiplies two integers
+ * @x: first integer
+ * @y: second integer
+ * @z: the product of two integers
+ * Return: returns an integer value z
+ */
+
+int mul(int x, int y)
+{
+	int z;
+
+	z = x * y;
+	return (z);
+}
+
---
+++ 0x04-more_functions_nested_loops/RAEDME.md
@@ -0,0 +1,3 @@
+#Write a function that checks for a digit (0 through 9).#\n #Write a function that multiplies two integers.#
+#Write a function that checks for a digit (0 through 9).#
+#Write a function that multiplies two integers.#
