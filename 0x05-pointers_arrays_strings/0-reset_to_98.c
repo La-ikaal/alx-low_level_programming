d453ab07023f61f37a8c8af772074a1037648dca
La-ikaal <scholasticaidewa@gmail.com>
2023-02-27 00:38:43 -0800

    0-reste_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
-/** 
- * reset_to_98 - reset the value of an interger  variable n
+/**
+ ** reset_to_98 - reset the value of an interger  variable n
  * @n : variable whose value is reset
  * Return: returns 0 at success
  */
