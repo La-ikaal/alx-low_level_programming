dfc6550cee0c9b31e0f03f0a8484a12103a141ac
La-ikaal <scholasticaidewa@gmail.com>
2023-02-28 02:56:24 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverses the order of a string
+ * @s : a variable containing the string to be reversed
+ * Return : always 0
+ */
+
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@
 #a function that swaps the values of two integers.
 # a function that returns the length of a string.
 #a function that prints a string, followed by a new line, to stdout.
+#a function that reverses a string.
